77cbed80f79ce5fb2f437f4df2605f887be8e77f
IvanArana <iv4ng4mer01@gmail.com>
2023-01-27 09:54:55 -0500

    Agregar archivos de proyecto.

---
+++ BitacorasAutocarIDYGS81/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BitacorasAutocarIDYGS81.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
